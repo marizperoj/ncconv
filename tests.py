8188a18fb2b21e4e6bda39cc3434bcf4be3e8eec
reid <reid@reid-Ubuntu>
2011-08-19 12:07:56 -0400

    Moved helprs and ncwrite into util package.
    updated tests.py

--- tests.py
+++ tests.py
@@ -4,29 +4,27 @@ from util.ncwrite import NcSpatial, NcTime, NcVariable, NcWrite
 import datetime
 import os
 from netCDF4 import Dataset
+import in_memory_oo_multi_core as ncconv
 
 
 class TestSimpleNc(unittest.TestCase):
 
-    def get_uri(self,constant=5,nlevels=1):
+    def get_uri(self,bounds=Polygon(((0,0),(10,0),(10,15),(0,15))),rng=[datetime.datetime(2007,10,1),datetime.datetime(2007,10,3)],res=5,constant=5,nlevels=1,path=None):
         """
         constant=5 -- provides a constant value when generating data to fill
             a NC variable. set to None to generate random values.
         nlevels=1 -- a value greater than 1 will create a NC with a fourth level
             dimension. the input values determines the number of levels.
         """
-        bounds = Polygon(((0,0),(10,0),(10,15),(0,15)))
-        res = 5
         ncspatial = NcSpatial(bounds,res)
         
-        rng = [datetime.datetime(2007,10,1),datetime.datetime(2007,10,3)]
         interval = datetime.timedelta(days=1)
         nctime = NcTime(rng,interval)
         
         ncvariable = NcVariable("Prcp","mm",constant=constant)
         
         ncw = NcWrite(ncvariable,ncspatial,nctime,nlevels=nlevels)
-        uri = ncw.write()
+        uri = ncw.write(path)
         return(uri)
 
     def test_get_uri(self):
@@ -34,9 +32,81 @@ class TestSimpleNc(unittest.TestCase):
         self.assertTrue(os.path.exists(uri))
         d = Dataset(uri,'r')
         sh = d.variables["Prcp"].shape
-        self.assertEqual(sh,(3,4,3))
+        self.assertEqual(sh,(3,3,2))
         d.close()
 
+
+
+    def _getCtrd(element):
+        g = element['geometry']
+        return (g.centroid.x,g.centroid.y)
+
+
+    def setUp(self):
+        self.singleLayer = self.get_uri(bounds=Polygon(((0,0),(20,0),(20,20),(0,20))),rng=[datetime.datetime(2007,1,1),datetime.datetime(2007,1,10)],res=10)
+        self.multiLayer = self.get_uri(nlevels=4)
+        self.randomized = self.get_uri(constant=None,nlevels=4)
+
+    def _access(self,uri,polygons,temporal,dissolve,clip,levels,subdivide,subres):
+        POLYINT = polygons
+        
+        NC = uri
+
+        TEMPORAL = temporal
+        DISSOLVE = dissolve
+        CLIP = clip
+        VAR = 'Prcp'
+        kwds = {
+            'rowbnds_name': 'latitude', 
+            'colbnds_name': 'longitude',
+            'time_units': 'days since 1800-01-01 00:00:00 0:00',
+            'level_name': 'level',
+            'calendar': 'gregorian'
+        }
+        LEVELS = [x for x in range(0,1)]
+        ## open the dataset for reading
+        dataset = NC#nc.Dataset(NC,'r')
+        ## make iterable if only a single polygon requested
+        if type(POLYINT) not in (list,tuple): POLYINT = [POLYINT]
+        ## convenience function for multiple polygons
+        elements = ncconv.multipolygon_multicore_operation(dataset,
+                                        VAR,
+                                        POLYINT,
+                                        time_range=TEMPORAL,
+                                        clip=CLIP,
+                                        dissolve=DISSOLVE,
+                                        levels = LEVELS,
+                                        ocgOpts=kwds,
+                                        subdivide=subdivide,
+                                        subres = subres
+                                        )
+
+        return elements
+
+    def test_LSSlSdRcd(self):
+        layer = self.singleLayer
+        poly = Polygon(((0,0),(10,0),(10,10),(0,10)))
+        time = [datetime.datetime(2000,1,1),datetime.datetime(2000,1,1)]
+        levels = None
+        subdivide = False
+        subres = 'Detect'
+        elements = self._access(layer,poly,time,False,False,levels,subdivide,subres)
+        #print elements
+        self.assertEqual(len(elements),1)
+        self.assertEqual(self._getCtrd(elements[0]),(5,5))
+        
+        poly = Polygon(((0,0),(20,0),(20,10),(0,10)))
+        elements = self.access(layer,poly,time,False,False,levels,subdivide,subres)
+        self.assertEqual(len(elements),2)
+
+        c = [self._getCtrd(x) for x in elements]
+        self.assertTrue((5,5) in c and (10,5) in c)
+
+        poly = Polygon(((0,0),(20,0),(20,20),(0,20)))
+        elements = self.access(layer,poly,time,False,False,levels,subdivide,subres)
+        self.assertEqual(len(elements),4)
+        c = [self._getCtrd(x) for x in elements]
+        self.assertTrue((5,5) in c and (10,5) in c and (10,10) in c and (5,10) in c)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
